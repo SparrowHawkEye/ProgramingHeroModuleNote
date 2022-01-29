4761e974ef0f91f6b730364cfb9f990c17b82bf1
sparrowhawkeyem <makh0043@gmail.com>
2022-01-29 17:50:37 +0600

    practice 19 added

---
+++ JS Practice/practice_19.js
@@ -0,0 +1,53 @@
+////// ******* 1. function to create multiplication table of 13 ******* /////
+
+function multiplicationTable(number) {
+  for (var i = 1; i <= 10; i++) {
+    console.log(number + "x" + i + " = " + number * i);
+  }
+}
+multiplicationTable(8);
+
+///// ******* 2. function to convert all case to lower Case ******* /////
+
+/*
+function convertToLower(name) {
+  let myName = name.toLowerCase();
+  return myName;
+}
+
+console.log(convertToLower("MaKsUd"));
+console.log(convertToLower("Jodu"));
+console.log(convertToLower("MODU"));
+console.log(convertToLower("kODu"));
+*/
+
+///// ******* 3. Creating a function using fullName which take two parameters firstName and lastName ******* /////
+/*
+function fullName(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+console.log(fullName("Maksudur", "Rahman"));
+console.log(fullName("Hablu", "Kanto"));
+*/
+///// ******* 4. function to find square of a number ******* /////
+
+/*
+
+function square(number) {
+  return number * number;
+}
+console.log(square(2));
+
+*/
+
+///// ******* 5. Create  a Object pizza to store the pizza details and print out pepperoni (Extra) ******* /////
+
+/*
+var pizza = {
+  toppings: ["cheese", "sauce", "pepperoni"],
+  crust: "deep dish",
+  serves: 2,
+};
+
+console.log(pizza.toppings[2]);
+*/
